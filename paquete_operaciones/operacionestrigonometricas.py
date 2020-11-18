f6f073c82f7363579968ac08e23bab2fd8ea3ac8
luis jose sanchez carrasco <luissancar@yahoo.es>
2020-11-18 11:23:47 +0100

    Add files via upload

---
+++ paquete_operaciones/operacionestrigonometricas.py
@@ -0,0 +1,7 @@
+import math
+
+def seno(i):
+    return math.sin(math.pi/i)
+
+def coseno(i):
+    return math.cos(math.pi/i)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="paqueteoperaciones",
+    version="1.0",
+    description="Paquete matemático",
+    author="tu nombre",
+    author_email="tu email",
+    url="tu url",
+    packages=["paquete_operaciones"]
+)
